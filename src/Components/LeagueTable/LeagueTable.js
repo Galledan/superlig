b6af4ad03caebe9a306c04a2aa647ea5764f6af8
Galledan <onrclklr@outlook.com>
2023-08-04 19:15:50 +0300

    table page responsive for phone

--- src/Components/LeagueTable/LeagueTable.js
+++ src/Components/LeagueTable/LeagueTable.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import "../../styles/table.scss"
+import "../../styles/table.scss";
 
 function LeagueTable() {
   const [tableData, setTableData] = useState([]);
@@ -11,7 +11,7 @@ function LeagueTable() {
         const response = await axios.get(
           "https://tffapi-1-y6918016.deta.app/live"
         );
-        setTableData(response.data)
+        setTableData(response.data);
       } catch (error) {
         console.error("Error fetching data:", error);
       }
@@ -27,13 +27,13 @@ function LeagueTable() {
           <tr>
             <th>#</th>
             <th>Takım</th>
-            <th>Oynanan</th>
-            <th>Galibiyet</th>
-            <th>Beraberlik</th>
-            <th>Mağlubiyet</th>
-            <th>AG</th>
-            <th>YG</th>
-            <th>A</th>
+            <th className="hidden-phone">Oynanan</th>
+            <th className="hidden-phone">Galibiyet</th>
+            <th className="hidden-phone">Beraberlik</th>
+            <th className="hidden-phone">Mağlubiyet</th>
+            <th className="hidden-phone">AG</th>
+            <th className="hidden-phone">YG</th>
+            <th className="hidden-phone">A</th>
             <th>Puan</th>
           </tr>
         </thead>
@@ -42,13 +42,13 @@ function LeagueTable() {
             <tr key={index}>
               <td>{index + 1}</td>
               <td className="team-name">{team.name}</td>
-              <td>{team.played}</td>
-              <td>{team.wins}</td>
-              <td>{team.draws}</td>
-              <td>{team.losses}</td>
-              <td>{team.goals_for}</td>
-              <td>{team.goals_against}</td>
-              <td>{team.average}</td>
+              <td className="hidden-phone">{team.played}</td>
+              <td className="hidden-phone">{team.wins}</td>
+              <td className="hidden-phone">{team.draws}</td>
+              <td className="hidden-phone">{team.losses}</td>
+              <td className="hidden-phone">{team.goals_for}</td>
+              <td className="hidden-phone">{team.goals_against}</td>
+              <td className="hidden-phone">{team.average}</td>
               <td>{team.points}</td>
             </tr>
           ))}
